532e80f4794de05cd22f7edea60f9db9d2c841c8
noura <nouranmontaser8991@gmail.com>
2018-12-13 03:07:36 +0200

    initial commit

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -60,12 +60,8 @@ public class ArticleDetailFragment extends Fragment implements
     private long mItemId;
     private View mRootView;
     private int mMutedColor = 0xFF333333;
-    //    private ObservableScrollView mScrollView;
-//    private DrawInsetsFrameLayout mDrawInsetsFrameLayout;
     private ColorDrawable mStatusBarColorDrawable;
     private Toolbar toolbar;
-    private int mTopInset;
-    //    private View mPhotoContainerView;
     private ImageView mPhotoView;
     private int mScrollY;
     private boolean mIsCard = false;
@@ -78,10 +74,6 @@ public class ArticleDetailFragment extends Fragment implements
     // Most time functions can only handle 1902 - 2037
     private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2, 1, 1);
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public ArticleDetailFragment() {
     }
 
@@ -108,18 +100,9 @@ public class ArticleDetailFragment extends Fragment implements
         setHasOptionsMenu(true);
     }
 
-//    public ArticleDetailActivity getActivityCast() {
-//        return (ArticleDetailActivity) getActivity();
-//    }
-
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        // In support library r8, calling initLoader for a fragment in a FragmentPagerAdapter in
-        // the fragment's onCreate may cause the same LoaderManager to be dealt to multiple
-        // fragments because their mIndex is -1 (haven't been added to the activity yet). Thus,
-        // we do this in onActivityCreated.
         getLoaderManager().initLoader(0, null, this);
     }
 
@@ -128,33 +111,16 @@ public class ArticleDetailFragment extends Fragment implements
                              Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
 
-        //        mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-//                mRootView.findViewById(R.id.draw_insets_frame_layout);
-//        mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
-//            @Override
-//            public void onInsetsChanged(Rect insets) {
-//                mTopInset = insets.top;
-//            }
-//        });
-
-        toolbar = (android.support.v7.widget.Toolbar) mRootView.findViewById(R.id.fragment_toolbarr);
+        toolbar = mRootView.findViewById(R.id.fragment_toolbarr);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
-//        mScrollView = (ObservableScrollView) mRootView.findViewById(R.id.scrollview);
-//        mScrollView.setCallbacks(new ObservableScrollView.Callbacks() {
-//            @Override
-//            public void onScrollChanged() {
-//                mScrollY = mScrollView.getScrollY();
-//                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-//                mPhotoContainerView.setTranslationY((int) (mScrollY - mScrollY / PARALLAX_FACTOR));
-//                updateStatusBar();
-//            }
-//        });
-
-        mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
-//        mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
+        mPhotoView =  mRootView.findViewById(R.id.photo);
 
         mStatusBarColorDrawable = new ColorDrawable(0);
+        ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        ab.setDisplayShowHomeEnabled(true); // show or hide the default home button
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
+        ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
 
         mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -167,29 +133,8 @@ public class ArticleDetailFragment extends Fragment implements
         });
 
         bindViews();
-//        updateStatusBar();
         return mRootView;
     }
-
-//    private void updateStatusBar() {
-//        int color = 0;
-//        if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
-//            float f = progress(mScrollY,
-//                    mStatusBarFullOpacityBottom - mTopInset * 3,
-//                    mStatusBarFullOpacityBottom - mTopInset);
-//            color = Color.argb((int) (255 * f),
-//                    (int) (Color.red(mMutedColor) * 0.9),
-//                    (int) (Color.green(mMutedColor) * 0.9),
-//                    (int) (Color.blue(mMutedColor) * 0.9));
-//        }
-//        mStatusBarColorDrawable.setColor(color);
-////        mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-//    }
-
-    static float progress(float v, float min, float max) {
-        return constrain((v - min) / (max - min), 0, 1);
-    }
-
     static float constrain(float val, float min, float max) {
         if (val < min) {
             return min;
@@ -216,40 +161,17 @@ public class ArticleDetailFragment extends Fragment implements
             return;
         }
 
-        TextView titleView = (TextView) mRootView.findViewById(R.id.article_title);
-//        TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
         collapsingToolbarLayout = (SubtitleCollapsingToolbarLayout) mRootView.findViewById(R.id.collapsingToolbar);
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
-
-
         bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
-        ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
         if (mCursor != null) {
             mRootView.setAlpha(0);
             mRootView.setVisibility(View.VISIBLE);
             mRootView.animate().alpha(1);
-//            int titleSize = mCursor.getString(ArticleLoader.Query.TITLE).length();
-//            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-//
-//            collapsingToolbarLayout.setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
-            Log.i("LLLLLLLL",mCursor.getString(ArticleLoader.Query.TITLE));
-//            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
             collapsingToolbarLayout.setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
-            ab.setDisplayShowHomeEnabled(true); // show or hide the default home button
-            ab.setDisplayHomeAsUpEnabled(true);
-            ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
-            ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
 
             Date publishedDate = parsePublishedDate();
             if (!publishedDate.before(START_OF_EPOCH.getTime())) {
-
-//                int subTitleSize = mCursor.getString(ArticleLoader.Query.AUTHOR).length();
-//                Spannable wordtoSpan = new SpannableString("\n"+mCursor.getString(ArticleLoader.Query.AUTHOR));
-//                wordtoSpan.setSpan(new StyleSpan(Typeface.NORMAL), titleSize, titleSize+wordtoSpan.length()+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                wordtoSpan.setSpan(new ForegroundColorSpan(Color.WHITE), titleSize, titleSize+wordtoSpan.length()+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                titleView.setText(wordtoSpan);
-
-//                titleView.append("\n");
                 collapsingToolbarLayout.setSubtitle(Html.fromHtml(
                         DateUtils.getRelativeTimeSpanString(
                                 publishedDate.getTime(),
@@ -258,31 +180,11 @@ public class ArticleDetailFragment extends Fragment implements
                                 + " by <font color='#ffffff'>"
                                 + mCursor.getString(ArticleLoader.Query.AUTHOR)
                                 + "</font>"));
-//                ((AppCompatActivity) getActivity()).getSupportActionBar().setSubtitle(Html.fromHtml(
-//                        DateUtils.getRelativeTimeSpanString(
-//                                publishedDate.getTime(),
-//                                System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-//                                DateUtils.FORMAT_ABBREV_ALL).toString()
-//                                + " by <font color='#ffffff'>"
-//                                + mCursor.getString(ArticleLoader.Query.AUTHOR)
-//                                + "</font>"));
-
             } else {
-                // If date is before 1902, just show the string
-//                titleView.append("\n");
                 collapsingToolbarLayout.setSubtitle(Html.fromHtml(
                         outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
                                 + mCursor.getString(ArticleLoader.Query.AUTHOR)
                                 + "</font>"));
-//                ((AppCompatActivity) getActivity()).getSupportActionBar().setSubtitle(Html.fromHtml(
-//                        outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
-//                                + mCursor.getString(ArticleLoader.Query.AUTHOR)
-//                                + "</font>"));
-//                int subTitleSize = mCursor.getString(ArticleLoader.Query.AUTHOR).length();
-//                Spannable wordtoSpan = new SpannableString("\n"+mCursor.getString(ArticleLoader.Query.AUTHOR));
-//                wordtoSpan.setSpan(new StyleSpan(Typeface.NORMAL), titleSize,titleSize+ wordtoSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                wordtoSpan.setSpan(new ForegroundColorSpan(Color.WHITE),  titleSize,titleSize+wordtoSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                titleView.setText(wordtoSpan);
             }
             bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY).replaceAll("(\r\n|\n)", "<br />")));
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
@@ -294,12 +196,8 @@ public class ArticleDetailFragment extends Fragment implements
                                 Palette p = Palette.generate(bitmap, 12);
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
-//                                mRootView.findViewById(R.id.meta_bar)
-//                                        .setBackgroundColor(mMutedColor);
-//                                updateStatusBar();
                             }
                         }
-
                         @Override
                         public void onErrorResponse(VolleyError volleyError) {
 
@@ -307,13 +205,6 @@ public class ArticleDetailFragment extends Fragment implements
                     });
         } else {
             mRootView.setVisibility(View.GONE);
-//            collapsingToolbarLayout.setTitle("N/A");
-//            toolbar.setSubtitle("N/A" );
-
-//            ab.setTitle("N/A");
-//            titleView.append("\n");
-//            ab.setSubtitle("N/A");
-
             collapsingToolbarLayout.setTitle("N/A");
             collapsingToolbarLayout.setSubtitle("N/A");
             bodyView.setText("N/A");
@@ -349,15 +240,4 @@ public class ArticleDetailFragment extends Fragment implements
         mCursor = null;
         bindViews();
     }
-
-//    public int getUpButtonFloor() {
-//        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
-//            return Integer.MAX_VALUE;
-//        }
-//
-//        // account for parallax
-//        return mIsCard
-//                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-//                : mPhotoView.getHeight() - mScrollY;
-//    }
 }
